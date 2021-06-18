aff269c8f35709c6cbe125ab589eb463ac49c54a
Satenik <karapetyanashkhen@gmail.com>
2021-06-18 12:18:35 +0200

    Control Flow

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,14 @@ namespace Grades
         {
             name = "Empty";
             grades = new List<float>();
+        }
+
+        internal void WriteGrades(TextWriter destination)
+        {
+            for (int i = grades.Count; i > 0; i--)
+            {
+                destination.WriteLine(grades[i - 1]);
+            }
         }
 
         public GradeStatistics ComputeStatistics()
@@ -41,13 +50,18 @@ namespace Grades
             }
             set
             {
-                if(!String.IsNullOrEmpty(value))
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                if (name != value && NameChanged != null)
                 {
                     NameChangedEventArgs args = new NameChangedEventArgs();
                     args.ExistingName = name;
                     args.NewName = value;
                     NameChanged(this, args);
                 }
+                name = value;
             }
         }
 
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 
 namespace Grades
 {
@@ -9,35 +9,62 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
 
-            book.NameChanged += OnNameChanged;
-            
-            book.Name = "Scott's Grade Book";
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
-
-            GradeStatistics stats = book.ComputeStatistics();
-            WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
-            WriteResult("Lowest", stats.LowestGrade);
-
         }
 
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        private static void GetBookName(GradeBook book)
         {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+            try
+            {
+                Console.WriteLine("Enter a name.");
+                book.Name = Console.ReadLine();
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-        
+
+        private static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}");
+        }
 
         static void WriteResult(string description, float result)
         {
             Console.WriteLine($"{description}: {result:F2}");
         }
 
-        static void WriteResult(string description, int result)
-        {
-            Console.WriteLine($"{description}: {result}");
-        }
+      
     }
 }
